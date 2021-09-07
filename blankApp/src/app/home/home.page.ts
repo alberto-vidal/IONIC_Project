648d545e26d7538f6d11e994fc15fa27260f4dd9
Alberto_Vidal <alberto.vidal.alvg@gmail.com>
2021-09-07 19:38:04 -0300

    Inicio de projeto - Hello World

---
+++ blankApp/src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+  clique(){
+    alert("Hello World");
+  }
+}
